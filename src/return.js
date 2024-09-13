c6e065ad7b20c09c0d147b0cb40cdc3083766c14
pb72yvp <143070157+Hugo652-droid@users.noreply.github.com>
2024-09-13 14:42:38 +0200

    feat: return asap

--- src/return.js
+++ src/return.js
@@ -1,8 +1,28 @@
-const numbers = [];
-for (let i = 0; i < 10; i++) {
-    numbers.push(Math.floor(Math.random() * 100));
+
+
+function calcul() {
+    const numbers = [];
+    let somme = 100;
+
+    for (let i = 0; i < 10; i++) {
+        numbers.push(Math.floor(Math.random() * 100));
+
+        if (i === 4) return"Vous étes à la moitier de la liste";
+
+        if (numbers[i] === numbers[i-1]) return "Il y a deux fois le même chiffre a la suite"
+
+        if (somme === 50) return "Vous avez 50 dans la soustraction";
+
+        if ((numbers[i] % 2) === 0) {
+            somme = somme - numbers[i];
+        }
+
+        for (let i = 0; i < numbers.length; i++) {
+            console.log(numbers[i]);
+        }
+    }
 }
 
-for (let i = 0; i < numbers.length; i++) {
-    console.log(numbers[i]);
-}
+console.log(calcul());
+
+
