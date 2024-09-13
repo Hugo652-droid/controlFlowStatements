30156e5b3cdc4291bb57a3e627bcff64e56027a2
pb72yvp <143070157+Hugo652-droid@users.noreply.github.com>
2024-09-13 14:16:41 +0200

    feat: continue better

--- src/continue.js
+++ src/continue.js
@@ -1,8 +1,16 @@
 const numbers = [];
+let somme = 0;
+let highNumber = 0;
+
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
+    if (numbers[i] <= highNumber) continue;
+    highNumber = numbers[i]
+    somme = somme + numbers[i];
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
 }
+
+console.log(somme)
