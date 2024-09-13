fb325cd39d6f52578500786ba7fd414589124d81
pb72yvp <143070157+Hugo652-droid@users.noreply.github.com>
2024-09-13 14:04:04 +0200

    feat:break on time

--- src/break.js
+++ src/break.js
@@ -1,8 +1,14 @@
 const numbers = [];
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
+    if (numbers[i]>50){
+        break; // le break gère le flux
+    }
 }
 
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
+
 }
+let skipedAction = 10 - numbers.length;
+console.log("L'app a éviter "+ skipedAction +" valeurs");
